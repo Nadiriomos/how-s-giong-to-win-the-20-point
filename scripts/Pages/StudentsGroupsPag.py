567aa2a2ece51f25d1fe31b0e11e48d2e38960bc
Nadiriomos <ywkuoamb@gmail.com>
2025-10-23 07:44:30 +0100

    fuck python its time for js

--- scripts/Pages/StudentsGroupsPag.py
+++
@@ -1,9 +0,0 @@
-# src/my_project/pages/dashboard.py
-from PySide6.QtWidgets import QWidget, QVBoxLayout, QLabel
-from PySide6.QtCore import Qt
-
-class StudentsGroupsPag(QWidget):
-    def __init__(self):
-        super().__init__()
-        layout = QVBoxLayout(self)
-        layout.addWidget(QLabel("students and groups Page", alignment=Qt.AlignCenter))
